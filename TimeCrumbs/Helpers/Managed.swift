a1932d2c96eb292b1cb646b1ade19c09d208b74a
Austin Goetz <a.goetz09@gmail.com>
2019-11-25 18:49:14 -0700

    builds out models and frames controller

---
+++ TimeCrumbs/Helpers/Managed.swift
@@ -0,0 +1,31 @@
+//
+//  Managed.swift
+//  TimeCrumbs
+//
+//  Created by Austin Goetz on 11/25/19.
+//  Copyright © 2019 Landon Epps. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+protocol Managed: class, NSFetchRequestResult {
+    static var entityName: String {get}
+    static var defaultSortDescriptors: [NSSortDescriptor] {get}
+}
+
+extension Managed {
+    static var defaultSortDescriptors: [NSSortDescriptor] {
+        return []
+    }
+    
+    static var sortedFetchRequest: NSFetchRequest<Self> {
+        let request = NSFetchRequest<Self>(entityName: entityName)
+        request.sortDescriptors = defaultSortDescriptors
+        return request
+    }
+}
+
+extension Managed where Self: NSManagedObject {
+    static var entityName: String {return entity().name!}
+}
---
+++ TimeCrumbs/Model Controllers/ProjectControllers.swift
@@ -0,0 +1,38 @@
+//
+//  ProjectControllers.swift
+//  TimeCrumbs
+//
+//  Created by Austin Goetz on 11/25/19.
+//  Copyright © 2019 Landon Epps. All rights reserved.
+//
+
+import Foundation
+
+class ProjectController {
+    
+    // MARK: - CRUD
+    // Create
+    func createProject(name: String, hourlyRate: Double, clientName: String = "", dateAdded: Date = Date()) {
+        
+    }
+    
+    // Update
+    func updateProject(project: Project, name: String, hourlyRate: Double, clientName: String) {
+        
+    }
+    
+    // Delete
+    func deleteProject(project: Project) {
+    
+    }
+    
+    // Archive
+    func archiveProject(project: Project) {
+        
+    }
+    
+    // Unarchive
+    func unarchiveProject(project: Project) {
+        
+    }
+}
