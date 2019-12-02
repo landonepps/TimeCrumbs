1011cbb5112eb3fd5bfbb2b42840ac93fe0c500a
Austin Goetz <a.goetz09@gmail.com>
2019-12-02 13:59:46 -0700

    builds out storyboards

---
+++ TimeCrumbs/View Controllers/Custom Cells/LogItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LogItemTableViewCell.swift
+//  TimeCrumbs
+//
+//  Created by Austin Goetz on 12/2/19.
+//  Copyright © 2019 Landon Epps. All rights reserved.
+//
+
+import UIKit
+
+class LogItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
