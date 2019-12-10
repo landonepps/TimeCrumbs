1955423e2017fecade9abe74ac594a62f35f11d8
Austin Goetz <a.goetz09@gmail.com>
2019-12-09 18:48:45 -0700

    adds segue from log to task edit

--- TimeCrumbs/View Controllers/LogTableViewController.swift
+++ TimeCrumbs/View Controllers/LogTableViewController.swift
@@ -94,15 +94,20 @@ class LogTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toLogTimeVC" {
+            guard let indexPath = tableView.indexPathForSelectedRow,
+                let task = fetchedResultsController?.object(at: indexPath),
+                let destination = segue.destination as? LogTimeViewController
+                else { return }
+            
+            destination.task = task
+            destination.project = task.project
+        }
     }
-    */
     
     // MARK: - Helpers
     
